ae98beb7e95225aaaafd80c7bc75228b9699f4b4
MisterAmara <khrocksss@gmail.com>
2025-06-01 14:07:35 -0400

    Update code.js

--- code.js
+++ code.js
@@ -1,17 +1,27 @@
 let display = document.getElementById("display");
 
 function appendNumber(number) {
-    display.value += number;
+  display.value += number;
+}
+
+function appendDecimal(dot) {
+  if (!display.value.includes(".")) {
+    display.value += dot;
+  }
 }
 
 function clearDisplay() {
-    display.value = '';
+  display.value = '';
+}
+
+function deleteLast() {
+  display.value = display.value.slice(0, -1);
 }
 
 function calculateResult() {
-    try {
-        display.value = eval(display.value);
-    } catch (e) {
-        display.value = "Error";
-    }
+  try {
+    display.value = eval(display.value);
+  } catch (e) {
+    display.value = "Error";
+  }
 }
